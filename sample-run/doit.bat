d4eaed908f817de868b895bb6c2455366bfe8c35
Mike Covington <mfcovington@gmail.com>
2016-02-16 00:31:50 -0800

    Add sample output files

---
+++ sample-run/doit.bat
@@ -0,0 +1,5 @@
+\Users\mfc\git.repos\COSOPT-Assembly-Line\bin\..\cosopt\cosoptL.new < cosoptL.in
+\Users\mfc\git.repos\COSOPT-Assembly-Line\bin\..\cosopt\cosopt2L.new < cosopt2L.in
+\Users\mfc\git.repos\COSOPT-Assembly-Line\bin\..\cosopt\cosopt3.new < cosopt3.in
+\Users\mfc\git.repos\COSOPT-Assembly-Line\bin\..\cosopt\cosopt4.new < cosopt4.in
+
